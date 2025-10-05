6e60405cb30fdea4764b903fc0f057e65373e32f
Rajkumar-robert <rajkumar09112003@gmail.com>
2025-10-05 15:26:37 +0530

    feat: enhance database connection logging with host and name details

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log('Database connected!!');
+        const db = await mongoose.connect(process.env.MONGODB_URI)
+        if(db) {
+          console.log('Database connected!!',db.connection.host,db.connection.name);
+        }
     }catch(error){
         console.error('Database connection failed:', error);
         process.exit(1);
